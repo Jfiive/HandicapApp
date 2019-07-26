7c7e069e7e3ad984cc5569827043e1d35ebc1594
Jonnie <jcamp@mestek.com>
2019-07-26 12:17:31 -0400

    Before Mainpage changes into hamburger menu

--- GolfHandicapApp/CourseSelection.xaml.cs
+++ GolfHandicapApp/CourseSelection.xaml.cs
@@ -12,9 +12,14 @@ namespace GolfHandicapApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CourseSelection : ContentPage
     {
+        private List<Course> mycourses = new List<Course>();
+        public List<Course> MyCourses { get { return mycourses; } }
         public CourseSelection()
         {
             InitializeComponent();
+            mycourses = App.Database.GetCourses();
+            CourseList.ItemsSource = mycourses;
+            ScoreDate.Date = DateTime.Today;
         }
 
         private void AddCourseButton_Clicked(object sender, EventArgs e)
@@ -32,6 +37,8 @@ namespace GolfHandicapApp
             CourseRating.Text = "";
 
             EnterCoursePopup.IsVisible = false;
+            mycourses = App.Database.GetCourses();
+            CourseList.ItemsSource = mycourses;
             //needs to reload the course selection list that will be here once it is done
         }
 
@@ -40,6 +47,29 @@ namespace GolfHandicapApp
             //might want to add an ActivityIndicator which is just a loading gif so that when this is up people know it is loading something
             //might also want to make it so that the popup closes if they click somewhere outside the popup
             EnterCoursePopup.IsVisible = true;
+        }
+
+        private void CourseList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //this needs to open the enter score popup so the person can post a score
+            var grid = (ListView)sender;
+            var selectedCourse = (Course)grid.SelectedItem;
+        }
+
+        private void NineHoles_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (EighteenHoles.IsChecked)
+            {
+                EighteenHoles.IsChecked = false;
+            }
+        }
+
+        private void EighteenHoles_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (NineHoles.IsChecked)
+            {
+                NineHoles.IsChecked = false;
+            }
         }
     }
 }
