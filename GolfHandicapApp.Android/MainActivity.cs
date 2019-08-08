da6860094107eb7cb16fed88ab3e8512d683c4e7
Jonnie <jcamp@mestek.com>
2019-08-08 08:57:29 -0400

    changed name of application for android

--- GolfHandicapApp.Android/MainActivity.cs
+++ GolfHandicapApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace GolfHandicapApp.Droid
 {
-    [Activity(Label = "GolfHandicapApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Handicap Calculator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
