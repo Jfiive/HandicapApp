b82d294bc2dfadd8799fed6c0d1f02bfe1a1122f
Jonnie <jcamp@mestek.com>
2019-08-13 10:18:57 -0400

    added a splash screen

---
+++ GolfHandicapApp.Android/SplashScreenActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GolfHandicapApp.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
