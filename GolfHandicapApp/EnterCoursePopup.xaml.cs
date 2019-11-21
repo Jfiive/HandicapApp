2a9fc3e58e605001971e22b35d066cc4144cd98b
Jonnie <jcamp@mestek.com>
2019-11-21 11:52:57 -0500

    comment clean up for last commit

--- GolfHandicapApp/EnterCoursePopup.xaml.cs
+++ GolfHandicapApp/EnterCoursePopup.xaml.cs
@@ -21,11 +21,6 @@ namespace GolfHandicapApp
             InitializeComponent();
             mp = m;
         }
-        //protected override void OnDisappearing()
-        //{
-        //    base.OnDisappearing();
-        //    mp.RefreshCourseList();
-        //}
         private void StatePicker_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (StatePicker.SelectedItem != null)
@@ -39,9 +34,6 @@ namespace GolfHandicapApp
             var item = (Course)e.SelectedItem;
             SelectedCourseID = item.CourseID;
             sender.Text = item.Name;
-            //TeePicker.ItemsSource = App.Database.GetCourseTees(item.CourseID);
-            //TeePicker.ItemDisplayBinding = new Binding("DisplayName");
-            //TeePicker.IsEnabled = true;
             ValidityCheck();
         }
 
@@ -56,16 +48,13 @@ namespace GolfHandicapApp
         {
             var NewCourse = new PlayedCourse();
             NewCourse.CourseID = SelectedCourseID;
-            //var TeeInfo = (PickerTee)TeePicker.SelectedItem;
-            //NewCourse.CourseID = TeeInfo.CourseID;
-            //NewCourse.InfoID = TeeInfo.InfoID;
             App.Database.SaveCourse(NewCourse);
             mp.RefreshCourseList();
             PopupNavigation.Instance.PopAllAsync();
         }
         private void ValidityCheck()
         {
-            if (SelectedCourseID > 0 /*&& TeePicker.SelectedIndex >= 0*/)
+            if (SelectedCourseID > 0)
             {
                 AddCourseButton.IsEnabled = true;
             }
@@ -74,10 +63,5 @@ namespace GolfHandicapApp
                 AddCourseButton.IsEnabled = false;
             }
         }
-
-        //private void TeePicker_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-        //    ValidityCheck();
-        //}
     }
 }
